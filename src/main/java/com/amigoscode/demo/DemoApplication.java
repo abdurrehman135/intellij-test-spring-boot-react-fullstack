c67b7d38b04971aa9e5072b8d81f22543428b02d
abdurrehman135 <abdurrehman135@gmail.com>
2024-11-09 01:21:56 -0500

    testing shortcuts

--- src/main/java/com/amigoscode/demo/DemoApplication.java
+++ src/main/java/com/amigoscode/demo/DemoApplication.java
@@ -8,5 +8,21 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
+        /* power of shortcuts:
+        Ctrl + Alt +
+            v - for extracting value to variable
+            n - to do opposite of above: return to in-line variable
+            m - for extracting to method
+        Ctrl + w
+            to extend your selection to a word and then to a whole sentence
+        */
+//        below just for demo purposes of above...
+//        printMessage("Hello How are you");
+//        printMessage("I am fine");
+//    }
+//
+//    private static void printMessage(String message) {
+//        System.out.println(message);
     }
 }
